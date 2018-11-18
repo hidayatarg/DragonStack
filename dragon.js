c236a6f459ffd2f79f63b71b30f48377182a6fe9
hidayatarg <hidayatarg@gmail.com>
2018-11-18 05:12:49 +0300

    Created the dragon class

---
+++ dragon.js
@@ -0,0 +1,10 @@
+class Dragon{
+    constructor(birthdate, nickname) {
+        // Variable that will be bellow this class
+        this.birthdate=birthdate;
+        this.nickname=nickname;
+    }
+}
+
+
+module.exports=Dragon;

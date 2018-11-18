1a8a1df46437cac9566b8b9d43c0c52134a07ee0
hidayatarg <hidayatarg@gmail.com>
2018-11-18 23:08:18 +0300

    render json traits

--- dragon.js
+++ dragon.js
@@ -1,17 +1,35 @@
+const TRAITS = require('./traits')
 // Screammers
 const DEFAULT_PROPERTIES = {
     nickname: 'unnamed',
     get birthdate(){
         return new Date()
+    },
+    get radomTraits(){
+        // Traits from json
+        const traits=[];        
+        TRAITS.forEach(TRAIT => {
+
+            traitType=TRAIT.type;
+            traitValues = TRAIT.values;
+
+            const traitValue = traitValues[
+                Math.floor(Math.random() * traitValues.length)
+            ];
+            traits.push({traitType, traitValue});
+
+        });
+        return traits;
     }
 }
 
 class Dragon{
     // We expect the send with keywords
-    constructor({birthdate, nickname}={}) {
+    constructor({birthdate, nickname, traits}={}) {
         // Variable that will be bellow this class
         this.birthdate=birthdate || DEFAULT_PROPERTIES.birthdate;
         this.nickname = nickname || DEFAULT_PROPERTIES.nickname;
+        this.traits = traits || DEFAULT_PROPERTIES.radomTraits;
     }
 }
 

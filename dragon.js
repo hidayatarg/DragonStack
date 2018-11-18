82c0fbf2d6d0f3f5fa8eebedb06398cef4ed43b2
hidayatarg <hidayatarg@gmail.com>
2018-11-18 05:16:34 +0300

    Even if we change the order

--- dragon.js
+++ dragon.js
@@ -1,5 +1,6 @@
 class Dragon{
-    constructor(birthdate, nickname) {
+    // We expect the send with keywords
+    constructor({birthdate, nickname}) {
         // Variable that will be bellow this class
         this.birthdate=birthdate;
         this.nickname=nickname;
--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const Dragon = require('./dragon');
 
-const fooey = new Dragon(new Date(), 'fooey');
-const baloo = new Dragon(new Date(), 'fooey');
+const fooey = new Dragon({ 
+    birthdate: new Date(), 
+    nickname: 'fooey'});
+
+const baloo = new Dragon({
+    nickname: 'baloo',
+    birthdate: new Date()
+});
+
 
 console.log('fooey', fooey);
 console.log('baloo', baloo);
